b364342176928191cc4dc8f118fb6d4b4ad328b8
Maksym-Sharafan <magsimsharafa@gmail.com>
2021-05-25 20:20:55 +0300

    Added check for empty string and space

--- src/index.js
+++ src/index.js
@@ -16,11 +16,27 @@ input.addEventListener('input', debounce(takeValueOnInput, 500));
 function takeValueOnInput(e) {
     clearContainer();
     let inputValue = e.target.value;
+
+    if (!input.value || input.value ===' ') {
+        return;
+    };
     
     fetchCountry(inputValue).then(countries => {
 
-            if (countries.length > 10) {
-                error({
+        if (/s^/) {
+            error({
+                    text: 'Remove the space at the beginning of the string!',
+                    mode: 'light',
+                    shadow: true,
+                    closer: true,
+                    sticker: false,
+                    hide: true,
+                    delay: 2000,
+                });
+        }
+
+        if (countries.length > 10) {
+             error({
                     text: 'Too many matches found. Please enter a more specific query!',
                     mode: 'light',
                     shadow: true,
@@ -30,13 +46,14 @@ function takeValueOnInput(e) {
                     delay: 2000,
                 });
                 return;
-            }
+        }
         
-            if (countries && countries.length > 1 && countries.length <= 10) {
+            if (countries.length >=2 && countries.length <= 10) {
                 renderCountryList(countries);
             }
-            if (countries && countries.length === 1) {
+            if (countries.length === 1) {
                 renderCountryCard(countries);
+                clearInput();
             }
         })
         .catch(showFetchError);
@@ -54,6 +71,10 @@ function renderCountryCard(countryName) {
 
 function clearContainer() {
     wrapperCountries.innerHTML = '';
+};
+
+function clearInput() {
+    input.value = '';
 };
 
 function showFetchError(err) {
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,6 @@
 const BASE_URL = 'https://restcountries.eu/rest/v2';
 
 export default function fetchCountries(searchQuery) {
-    if (searchQuery === '') return;
     
    return fetch(`${BASE_URL}/name/${searchQuery}`)
         .then(response => {
